a5b42a2fe9980310dd798f42d870a4eae0f57414
James O'Grady <JAMOGRAD@uk.ibm.com>
2023-01-16 16:08:13 +0000

    Reduce Cognitive Complexity.... at any cost!

--- src/webui/src/main/java/com/ibm/cics/cip/bankliberty/webui/data_access/AccountList.java
+++ src/webui/src/main/java/com/ibm/cics/cip/bankliberty/webui/data_access/AccountList.java
@@ -82,24 +82,6 @@ public class AccountList {
 				BigDecimal balance = BigDecimal.valueOf(new Double(filter.substring(34)));
 
 				myAccountsResponse = myAccountsResource.getAccountsByBalanceWithOffsetAndLimitExternal(balance,operator,null,null,true);
-
-				if(myAccountsResponse.getStatus() == 200)
-				{
-
-					String myAccountsString = myAccountsResponse.getEntity().toString();
-
-
-					JSONObject myAccountsJSON = JSONObject.parse(myAccountsString);
-					long accountCount = (Long) myAccountsJSON.get(JSON_NUMBER_OF_ACCOUNTS);
-					this.count = (int) accountCount;
-
-
-				}
-				else
-				{
-					this.count = 0;
-				}
-
 			}
 
 			if(filter.contains("AND ACCOUNT_NUMBER"))
@@ -112,11 +94,6 @@ public class AccountList {
 				}
 				Long accountNumberFilterLong = new Long(accountNumberFilter);
 				myAccountsResponse = myAccountsResource.getAccountExternal(accountNumberFilterLong);
-				this.count = 0;
-				if(myAccountsResponse.getStatus() == 200)
-				{
-					this.count = 1;
-				}
 			}
 
 			if(filter.contains("AND ACCOUNT_CUSTOMER_NUMBER"))
@@ -126,16 +103,6 @@ public class AccountList {
 				Long customerNumber = new Long(customerNumberFilter);
 
 				myAccountsResponse = myAccountsResource.getAccountsByCustomerExternal(customerNumber,true);
-				String myAccountsString = myAccountsResponse.getEntity().toString();
-				JSONObject myAccountsJSON;
-				this.count = 0;
-				if(myAccountsResponse.getStatus() == 200)
-				{
-					myAccountsJSON = JSONObject.parse(myAccountsString);
-					long accountCount = (Long) myAccountsJSON.get(JSON_NUMBER_OF_ACCOUNTS);
-					this.count = (int) accountCount;
-					//("Account_customer_number has " + accountCount + " accounts");
-				}
 
 			}
 
@@ -143,15 +110,17 @@ public class AccountList {
 			{
 				//("No filter so get the lot please!");
 				myAccountsResponse = myAccountsResource.getAccountsExternal(true);
-				String myAccountsString = myAccountsResponse.getEntity().toString();
-				JSONObject myAccountsJSON;
 				this.count = 0;
-				if(myAccountsResponse.getStatus() == 200)
-				{
-					myAccountsJSON = JSONObject.parse(myAccountsString);
-					long accountCount = (Long) myAccountsJSON.get(JSON_NUMBER_OF_ACCOUNTS);
-					this.count = (int) accountCount;
-				}
+			}
+			if(myAccountsResponse.getStatus() == 200)
+			{
+
+				String myAccountsString = myAccountsResponse.getEntity().toString();
+
+
+				JSONObject myAccountsJSON = JSONObject.parse(myAccountsString);
+				long accountCount = (Long) myAccountsJSON.get(JSON_NUMBER_OF_ACCOUNTS);
+				this.count = (int) accountCount;
 			}
 		}
 		catch (IOException e) 
@@ -167,210 +136,66 @@ public class AccountList {
 
 		AccountsResource myAccountsResource = new AccountsResource();
 
-
-
-		//("Doing get for limit=" + limit + ",offset=" + offset + ",filter=" + filter);
 		Response myAccountsResponse = null;
 		String myAccountsString = null;
 		JSONObject myAccountsJSON = null;
-
-		if(filter.contains(" AND ACCOUNT_AVAILABLE_BALANCE"))
+		
+		try
 		{
-			this.accountList.clear();
-			//01234567890123456789012345678901234567890
-			// AND ACCOUNT_AVAILABLE_BALANCE <= 33558.0
-			//("We are filtering by balance");
-			//.out.println(filter);
-			String operator = filter.substring(31,32);
-			BigDecimal balance = BigDecimal.valueOf(new Double(filter.substring(34)));
 
-			myAccountsResponse = myAccountsResource.getAccountsByBalanceWithOffsetAndLimitExternal(balance,operator, offset, limit,false);
+			if(filter.contains(" AND ACCOUNT_AVAILABLE_BALANCE"))
+			{
+				this.accountList.clear();
+				String operator = filter.substring(31,32);
+				BigDecimal balance = BigDecimal.valueOf(new Double(filter.substring(34)));
 
-			//("myAccountsResponse is " + myAccountsResponse.getStatus());
+				myAccountsResponse = myAccountsResource.getAccountsByBalanceWithOffsetAndLimitExternal(balance,operator, offset, limit,false);
+			}
 
+
+
+			if(filter.contains(" AND ACCOUNT_NUMBER"))
+			{
+				this.accountList.clear();
+
+				String accountNumberFilter = filter.substring(22);
+				Long accountNumberFilterLong = new Long(accountNumberFilter);
+
+				myAccountsResponse = myAccountsResource.getAccountExternal(accountNumberFilterLong);
+			}
+
+
+
+			//                  0123456789012345678901234567890
+			if(filter.contains(" AND ACCOUNT_CUSTOMER_NUMBER = "))
+			{
+				this.accountList.clear();
+				String customerNumberFilter = filter.substring(31);
+				Long customerNumber = new Long(customerNumberFilter);
+
+				myAccountsResponse = myAccountsResource.getAccountsByCustomerExternal(customerNumber,false);
+			}
+
+			if(filter.length() == 0)
+			{
+				this.accountList.clear();
+				myAccountsResponse = myAccountsResource.getAccountsExternal(limit,offset,false);
+			}
+
+			if(offset == 0){
+				//("Offset is zero to doing howMany for filter " + filter);
+				howMany(filter);
+			}
 			myAccountsString = myAccountsResponse.getEntity().toString();
 
 			if(myAccountsResponse.getStatus() == 200)
 			{
-				try {
-					myAccountsJSON = JSONObject.parse(myAccountsString);
-					long accountCount = (Long) myAccountsJSON.get(JSON_NUMBER_OF_ACCOUNTS);
-					this.count = (int) accountCount;
-					JSONArray myAccountsArrayJSON = (JSONArray) myAccountsJSON.get(JSON_ACCOUNTS);
 
-					//("accountCount is " + accountCount + " myAccountsArrayJSON is " + myAccountsArrayJSON); 
-					for(int i = 0;i < accountCount;i++)
-					{
-						JSONObject myAccount = (JSONObject) myAccountsArrayJSON.get(i);
-
-						Date lastStatement = sortOutDate((String) myAccount.get(JSON_LAST_STATEMENT_DATE));
-						Date nextStatement = sortOutDate((String) myAccount.get(JSON_NEXT_STATEMENT_DATE));
-						Date dateOpened = sortOutDate((String) myAccount.get(JSON_DATE_OPENED));
-						String id = (String)myAccount.get(JSON_ID);
-						String customerNumberString = (String) myAccount.get(JSON_CUSTOMER_NUMBER);
-					
-						BigDecimal actual_balance = BigDecimal.valueOf((Double) myAccount.get(JSON_ACTUAL_BALANCE)).setScale(2,RoundingMode.HALF_UP);
-						BigDecimal available_balance = BigDecimal.valueOf((Double) myAccount.get(JSON_AVAILABLE_BALANCE)).setScale(2,RoundingMode.HALF_UP);
-						BigDecimal interest_rate = BigDecimal.valueOf((Double) myAccount.get(JSON_INTEREST_RATE)).setScale(2,RoundingMode.HALF_UP);
-						Long overdraft = (Long) myAccount.get(JSON_OVERDRAFT);
-						String sortCode = (String) myAccount.get(JSON_SORT_CODE);
-						String type = (String) myAccount.get(JSON_ACCOUNT_TYPE);
-
-
-						Account myListAccount = new Account();
-						myListAccount.setAccountNumber(id);
-						myListAccount.setActualBalance(actual_balance.setScale(2,RoundingMode.HALF_UP));
-						myListAccount.setAvailableBalance(available_balance.setScale(2,RoundingMode.HALF_UP));
-						myListAccount.setCustomerNumber(customerNumberString);
-						myListAccount.setInterestRate(interest_rate.setScale(2,RoundingMode.HALF_UP));
-						myListAccount.setLastStatement(lastStatement);
-						myListAccount.setNextStatement(nextStatement);
-						myListAccount.setOpened(dateOpened);
-						myListAccount.setOverdraftLimit(overdraft.intValue());
-						myListAccount.setSortcode(sortCode);
-						myListAccount.setType(type);
-
-						this.accountList.add(myListAccount);
-					}
-
-				} catch (IOException e) {
-					logger.severe(e.toString());
-				}
-			}
-		}
-
-
-
-		if(filter.contains(" AND ACCOUNT_NUMBER"))
-		{
-			this.accountList.clear();
-			//("We are filtering by account");
-			String accountNumberFilter = filter.substring(22);
-			Long accountNumberFilterLong = new Long(accountNumberFilter);
-			//("We are filtering by account " + accountNumberFilterLong);
-			myAccountsResponse = myAccountsResource.getAccountExternal(accountNumberFilterLong);
-			if(myAccountsResponse.getStatus() == 200)
-			{
-				myAccountsString = myAccountsResponse.getEntity().toString();
 				myAccountsJSON = JSONObject.parse(myAccountsString);
-				this.count = 1;
-				JSONObject myAccount = (JSONObject) myAccountsJSON;
-
-				Date lastStatement = sortOutDate((String) myAccount.get(JSON_LAST_STATEMENT_DATE));
-				Date nextStatement = sortOutDate((String) myAccount.get(JSON_NEXT_STATEMENT_DATE));
-				Date dateOpened = sortOutDate((String) myAccount.get(JSON_DATE_OPENED));
-				String id = (String)myAccount.get(JSON_ID);
-				String customerNumber = (String) myAccount.get(JSON_CUSTOMER_NUMBER);
-				BigDecimal actual_balance = BigDecimal.valueOf((Double) myAccount.get(JSON_ACTUAL_BALANCE)).setScale(2,RoundingMode.HALF_UP);
-				BigDecimal available_balance = BigDecimal.valueOf((Double) myAccount.get(JSON_AVAILABLE_BALANCE)).setScale(2,RoundingMode.HALF_UP);
-				BigDecimal interest_rate = BigDecimal.valueOf((Double) myAccount.get(JSON_INTEREST_RATE)).setScale(2,RoundingMode.HALF_UP);
-				Long overdraft = (Long) myAccount.get(JSON_OVERDRAFT);
-				String sortCode = (String) myAccount.get(JSON_SORT_CODE);
-				String type = (String) myAccount.get(JSON_ACCOUNT_TYPE);
-
-
-				Account myListAccount = new Account();
-				myListAccount.setAccountNumber(id);
-				myListAccount.setActualBalance(actual_balance.setScale(2,RoundingMode.HALF_UP));
-				myListAccount.setAvailableBalance(available_balance.setScale(2,RoundingMode.HALF_UP));
-				myListAccount.setCustomerNumber(customerNumber);
-				myListAccount.setInterestRate(interest_rate.setScale(2,RoundingMode.HALF_UP));
-				myListAccount.setLastStatement(lastStatement);
-				myListAccount.setNextStatement(nextStatement);
-				myListAccount.setOpened(dateOpened);
-				myListAccount.setOverdraftLimit(overdraft.intValue());
-				myListAccount.setSortcode(sortCode);
-				myListAccount.setType(type);
-
-				this.accountList.add(myListAccount);
-			}
-		}
-
-
-
-		//                  0123456789012345678901234567890
-		if(filter.contains(" AND ACCOUNT_CUSTOMER_NUMBER = "))
-		{
-			this.accountList.clear();
-			//("filter is '" + filter + "'");
-			//("We are filtering by customer number!");
-			String customerNumberFilter = filter.substring(31);
-			Long customerNumber = new Long(customerNumberFilter);
-
-			myAccountsResponse = myAccountsResource.getAccountsByCustomerExternal(customerNumber,false);
-
-
-			myAccountsString = myAccountsResponse.getEntity().toString();
-
-			//("myAccountsResponse status in account_customer_number is " + myAccountsResponse.getStatus());
-			if(myAccountsResponse.getStatus() == 200)
-			{
-				try {
-					myAccountsJSON = JSONObject.parse(myAccountsString);
-					long accountCount = (Long) myAccountsJSON.get(JSON_NUMBER_OF_ACCOUNTS);
-					this.count = (int) accountCount;
-					JSONArray myAccountsArrayJSON = (JSONArray) myAccountsJSON.get(JSON_ACCOUNTS);
-
-					//("accountCount is " + accountCount);
-					if(myAccountsArrayJSON.size() > 0)
-					{
-						//("myAccountsArrayJSON.size()  is " +myAccountsArrayJSON.size() );
-						for(int i = 0;i < accountCount;i++)
-						{
-							//("i is " + i + " and accountCount is " + accountCount);
-							JSONObject myAccount = (JSONObject) myAccountsArrayJSON.get(i);
-
-							Date lastStatement = sortOutDate((String) myAccount.get(JSON_LAST_STATEMENT_DATE));
-							Date nextStatement = sortOutDate((String) myAccount.get(JSON_NEXT_STATEMENT_DATE));
-							Date dateOpened = sortOutDate((String) myAccount.get(JSON_DATE_OPENED));
-							String id = (String)myAccount.get(JSON_ID);
-							String customerNumberString = (String) myAccountsJSON.get(JSON_CUSTOMER_NUMBER);
-							BigDecimal actual_balance = BigDecimal.valueOf((Double) myAccount.get(JSON_ACTUAL_BALANCE)).setScale(2,RoundingMode.HALF_UP);
-							BigDecimal available_balance = BigDecimal.valueOf((Double) myAccount.get(JSON_AVAILABLE_BALANCE)).setScale(2,RoundingMode.HALF_UP);
-							BigDecimal interest_rate = BigDecimal.valueOf((Double) myAccount.get(JSON_INTEREST_RATE)).setScale(2,RoundingMode.HALF_UP);
-							Long overdraft = (Long) myAccount.get(JSON_OVERDRAFT);
-							String sortCode = (String) myAccount.get(JSON_SORT_CODE);
-							String type = (String) myAccount.get(JSON_ACCOUNT_TYPE);
-
-
-							Account myListAccount = new Account();
-							myListAccount.setAccountNumber(id);
-							myListAccount.setActualBalance(actual_balance.setScale(2,RoundingMode.HALF_UP));
-							myListAccount.setAvailableBalance(available_balance.setScale(2,RoundingMode.HALF_UP));
-							myListAccount.setCustomerNumber(customerNumberString);
-							myListAccount.setInterestRate(interest_rate.setScale(2,RoundingMode.HALF_UP));
-							myListAccount.setLastStatement(lastStatement);
-							myListAccount.setNextStatement(nextStatement);
-							myListAccount.setOpened(dateOpened);
-							myListAccount.setOverdraftLimit(overdraft.intValue());
-							myListAccount.setSortcode(sortCode);
-							myListAccount.setType(type);
-
-							this.accountList.add(myListAccount);
-						}
-					}
-
-				} 
-				catch (IOException e) 
-				{
-					logger.severe(e.toString());
-				}
-			}
-		}
-
-		if(filter.length() == 0)
-		{
-			this.accountList.clear();
-			//("No filter so just get the lot? What about offset " + offset + " and limit " + limit + "?");
-			myAccountsResponse = myAccountsResource.getAccountsExternal(limit,offset,false);
-			myAccountsString = myAccountsResponse.getEntity().toString();
-			myAccountsJSON = JSONObject.parse(myAccountsString);
-
-
-			if(myAccountsResponse.getStatus() == 200)
-			{
 				long accountCount = (Long) myAccountsJSON.get(JSON_NUMBER_OF_ACCOUNTS);
+				this.count = (int) accountCount;
 				JSONArray myAccountsArrayJSON = (JSONArray) myAccountsJSON.get(JSON_ACCOUNTS);
+
 
 				for(int i = 0;i < accountCount;i++)
 				{
@@ -380,7 +205,8 @@ public class AccountList {
 					Date nextStatement = sortOutDate((String) myAccount.get(JSON_NEXT_STATEMENT_DATE));
 					Date dateOpened = sortOutDate((String) myAccount.get(JSON_DATE_OPENED));
 					String id = (String)myAccount.get(JSON_ID);
-					String customerNumber = (String) myAccount.get(JSON_CUSTOMER_NUMBER);
+					String customerNumberString = (String) myAccount.get(JSON_CUSTOMER_NUMBER);
+
 					BigDecimal actual_balance = BigDecimal.valueOf((Double) myAccount.get(JSON_ACTUAL_BALANCE)).setScale(2,RoundingMode.HALF_UP);
 					BigDecimal available_balance = BigDecimal.valueOf((Double) myAccount.get(JSON_AVAILABLE_BALANCE)).setScale(2,RoundingMode.HALF_UP);
 					BigDecimal interest_rate = BigDecimal.valueOf((Double) myAccount.get(JSON_INTEREST_RATE)).setScale(2,RoundingMode.HALF_UP);
@@ -393,7 +219,7 @@ public class AccountList {
 					myListAccount.setAccountNumber(id);
 					myListAccount.setActualBalance(actual_balance.setScale(2,RoundingMode.HALF_UP));
 					myListAccount.setAvailableBalance(available_balance.setScale(2,RoundingMode.HALF_UP));
-					myListAccount.setCustomerNumber(customerNumber);
+					myListAccount.setCustomerNumber(customerNumberString);
 					myListAccount.setInterestRate(interest_rate.setScale(2,RoundingMode.HALF_UP));
 					myListAccount.setLastStatement(lastStatement);
 					myListAccount.setNextStatement(nextStatement);
@@ -404,12 +230,13 @@ public class AccountList {
 
 					this.accountList.add(myListAccount);
 				}
-			}
-		}
 
-		if(offset == 0){
-			//("Offset is zero to doing howMany for filter " + filter);
-			howMany(filter);
+			}
+
+		}
+		catch (IOException e) 
+		{
+			logger.severe(e.toString());
 		}
 	}
 
