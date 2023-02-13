e8b5e4dfabe113f94fa42c0b4270c204700d9abe
James O'Grady <JAMOGRAD@uk.ibm.com>
2023-02-13 16:05:32 +0000

    Down with deferredjs!
    what does it do? Take it out, still works...

--- src/webui/WebContent/VAADIN/widgetsets/webui.widgetset.webuiWidgetset/deferredjs/84363BFA8D1ACE40C6650B0CADF5C9B8/2.cache.js
+++
@@ -1,4 +0,0 @@
-/*                                                                        */
-/* Copyright IBM Corp. 2023                                               */
-/*                                                                        */
-$wnd.com_example_com_ibm_cics_cip_bank_libertyapi_webui_widgetset_Com_ibm_cics_cip_bank_libertyapi_webuiWidgetset.runAsyncCallback2('beb(1615,1,L2d);_.vc=function Uic(){F3b((!y3b&&(y3b=new K3b),y3b),this.a.d)};gYd(Th)(2);\n//# sourceURL=com.example.com_ibm_cics_cip_bank_libertyapi_webui.widgetset.Com_ibm_cics_cip_bank_libertyapi_webuiWidgetset-2.js\n')
